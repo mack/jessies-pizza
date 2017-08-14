1151da37e8c2885f3a07b2b3f988514bbadfa478
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-08-14 20:04:58 -0300

    Added in stripe files

---
+++ stripe/pay.php
@@ -0,0 +1,16 @@
+
+
+<form action="transaction.php" method="POST">
+  test
+  <script
+    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+    data-key="<?php $configs = include('config.php'); echo $configs['stripeKey']?>"
+    data-amount="999"
+    data-name="Mackenzie Boudreau"
+    data-description="Widget"
+    data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+    data-locale="auto"
+    data-zip-code="true"
+    data-currency="cad">
+  </script>
+</form>
--- stripe/transaction.php
+++ stripe/transaction.php
@@ -1,6 +1,6 @@
 <?php
+$configs = include('config.php');
+//$token = $_POST['stripeToken'];
 
-
-
-
+echo $configs['stripeKey'];
 ?>
