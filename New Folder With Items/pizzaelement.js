2103ce5773835b50d546c7b7e040281c12cbbe52
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-08-15 21:39:39 -0300

    Added in divs

---
+++ New Folder With Items/pizzaelement.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var c=document.getElementById("myCanvas");
+    console.log(c)
+    // var ctx=c.getContext("2d");
+    // var img="pizza.png"
+    // ctx.drawImage(img,10,10);
+};
